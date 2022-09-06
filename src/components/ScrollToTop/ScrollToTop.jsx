01c7efea088345d5780dd836254f02ccd2373051
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-07 00:20:58 +0900

    Feat: ScrollToTop component 생성
    스크롤을 맨위로 올려주는 버튼

---
+++ src/components/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+import { ScrollToTopBtn, UpperImg } from '@/components/ScrollToTop/ScrollToTop.styled';
+import imgUp from '@/assets/images/ic-up.png';
+
+const ScrollToTop = () => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleVisible = () => {
+    const { scrollY } = window;
+    if (scrollY > 300) return setVisible(true);
+    return setVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisible);
+    return () => window.removeEventListener('scroll', toggleVisible);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <ScrollToTopBtn onClick={scrollToTop} isDisplay={visible}>
+      <UpperImg src={imgUp} />
+    </ScrollToTopBtn>
+  );
+};
+
+export default ScrollToTop;
