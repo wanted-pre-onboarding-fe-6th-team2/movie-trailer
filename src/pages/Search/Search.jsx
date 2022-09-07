90126c8a3801b09f9df205896b797ff41843bfe5
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-08 04:55:53 +0900

    Chore: 자동완성 기능을 위한 Recommendation 컴포넌트 삭제
    - 시간 부족으로 인한 추가 기능으로 연기
    - 코딩 컨벤션에 맞게 page contianer 삭제

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import SearchInput from '@/components/search/SearchInput/SearchInput';
-import PageContainer from '@/components/common/PageContainer/PageContainer';
 
-const SearchPage = () => (
-  <div>
-    <PageContainer>
-      <SearchInput />
-    </PageContainer>
-  </div>
-);
+const SearchPage = () => <SearchInput />;
 
 export default SearchPage;
