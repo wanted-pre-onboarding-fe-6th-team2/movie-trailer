8214e24c30aff6cb494cff6d69444e743ced7fad
DESKTOP-ICDGUQJ\sunghwa <tjdghk3920@naver.com>
2022-09-07 14:26:20 +0900

    Feat: throttle을 통한 이벤트 관리

--- src/components/common/ScrollToTop/ScrollToTop.jsx
+++ src/components/common/ScrollToTop/ScrollToTop.jsx
@@ -3,18 +3,36 @@ import { ScrollToTopBtn, UpperImg } from '@/components/common/ScrollToTop/Scroll
 import imgUp from '@/assets/images/ic-up.png';
 
 const ScrollToTop = () => {
-  const [visible, setVisible] = useState(false);
+  const [visibleToTopBtn, setVisibleToTopBtn] = useState(false);
 
-  const toggleVisible = () => {
-    const { scrollY } = window;
-    if (scrollY > 300) return setVisible(true);
-    return setVisible(false);
+  const throttle = (handler, timeout = 300) => {
+    let invokedTime;
+    let timer;
+
+    return () => {
+      if (!invokedTime) {
+        handler.apply(null);
+        invokedTime = Date.now();
+      } else {
+        clearTimeout(timer);
+        timer = window.setTimeout(() => {
+          handler.apply(null);
+          invokedTime = Date.now();
+        }, Math.max(timeout - (Date.now() - invokedTime), 0));
+      }
+    };
   };
+
+  const toggleVisible = throttle(() => {
+    const { scrollY } = window;
+    if ((scrollY > 300 && visibleToTopBtn) || (scrollY <= 300 && !visibleToTopBtn)) return;
+    setVisibleToTopBtn(prev => !prev);
+  });
 
   useEffect(() => {
     window.addEventListener('scroll', toggleVisible);
     return () => window.removeEventListener('scroll', toggleVisible);
-  }, []);
+  }, [toggleVisible]);
 
   const scrollToTop = () => {
     window.scrollTo({
@@ -24,7 +42,7 @@ const ScrollToTop = () => {
   };
 
   return (
-    <ScrollToTopBtn onClick={scrollToTop} isDisplay={visible}>
+    <ScrollToTopBtn onClick={scrollToTop} isDisplay={visibleToTopBtn}>
       <UpperImg src={imgUp} />
     </ScrollToTopBtn>
   );
