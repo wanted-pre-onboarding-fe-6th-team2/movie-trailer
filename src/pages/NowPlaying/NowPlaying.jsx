5cd35a1e67294fd3f58d15167b28c683a9809fd6
joohe71 <joohe71@gmail.com>
2022-09-07 19:33:38 +0900

    Refactor : api 요청 로직에서 useSWR 적용

--- src/pages/NowPlaying/NowPlaying.jsx
+++ src/pages/NowPlaying/NowPlaying.jsx
@@ -1,27 +1,25 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import useSWR from 'swr';
 import MovieApiService from '@/api/movieService';
 import * as Styled from '@/pages/NowPlaying/NowPlaying.styled';
 import MovieListItem from '@/components/MovieListItem/MovieListItem';
 import PageContainer from '@/components/common/PageContainer/PageContainer';
 
 const NowPlaying = () => {
-  const [movieLists, setMovieLists] = useState([]);
+  const { data, error } = useSWR({ page: 1 }, MovieApiService.getNowPlayingMovies);
 
-  const getMovieLists = async () => {
-    const response = await MovieApiService.getNowPlayingMovies({ page: 1 });
-    const listResults = response.results;
-    setMovieLists(listResults);
-  };
-  useEffect(() => {
-    getMovieLists(true);
-  }, []);
+  if (error) {
+    console.log('에러 발생');
+  } else if (!data) {
+    console.log('로딩중입니다.');
+  }
 
   return (
     <PageContainer>
       <Styled.MoviePage>
         <Styled.SubTitle>현재 상영작</Styled.SubTitle>
         <Styled.MovieList>
-          {movieLists.map(list => (
+          {data?.results.map(list => (
             <MovieListItem
               key={list.id}
               id={list.id}
