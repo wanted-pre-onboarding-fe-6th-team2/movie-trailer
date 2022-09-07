2798bd788c0597b64ae665f941965c8aa1028a66
yeongjuyun <myyun1029@gmail.com>
2022-09-07 17:07:14 +0900

    Feat: MovieDetail 페이지 생성

---
+++ src/pages/MovieDetail/MovieDetail.styled.js
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+const MovieDetailContainer = styled.div`
+  width: 1100px;
+  min-height: 500px;
+  border: 1px solid black;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export { MovieDetailContainer };
