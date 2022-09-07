10f2df03954bfb846562712c9cb84f990ea0d00c
HaJunRyu <hjr4557@naver.com>
2022-09-07 17:40:57 +0900

    Feat: 영화 상세 페이지에서 사용 할 api를 추가한다
    영화 상세 페이지에서 영상을 직접적으로 보여줘야하기 때문에 영상 링크를 응답해주는 api를 추가한다

--- src/constants/swr.js
+++ src/constants/swr.js
@@ -1,5 +1,6 @@
 export const USE_SWR_KEYS = {
   MOVIE_DETAIL: 'movie-detail',
+  MOVIE_VIDEOS: 'movie-videos',
   MOVIE_SEARCH: 'movie-search',
   NOW_PLAYING_MOVIES: 'now-playing-movies',
   POPULAR_MOVIES: 'popular-movies',
