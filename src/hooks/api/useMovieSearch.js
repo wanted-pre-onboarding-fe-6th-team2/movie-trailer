a81e7a9c66a7d4206f7728b756a1d728545fcbde
HaJunRyu <hjr4557@naver.com>
2022-09-08 03:35:33 +0900

    Feat: SWR api hook을 무한스크롤을 통해 fetch할 수 있게 고도화한다
    SWR을 이용해 만들어준 api를 랩핑한 hook들 중에 page단위로 fetch할 수 있는 hook들에 useSWRInfinify와 useInfinity custom hook을 사용하여 무한스크롤 시 지속적으로 fetch을 할 수 있도록 고도화 해주었습니다.

--- src/hooks/api/useMovieSearch.js
+++ src/hooks/api/useMovieSearch.js
@@ -1,16 +1,27 @@
-import useSWR from 'swr';
+import useSWRInfinite from 'swr/infinite';
 import searchApiService from '@/api/searchService';
 import { USE_SWR_KEYS } from '@/constants/swr';
+import { generateSwrGetKeyFunction } from '@/utils/swr';
+import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
 
-const useMovieSearch = ({ searchKeyword, language, page } = {}) => {
-  const { data, error } = useSWR(USE_SWR_KEYS.MOVIE_SEARCH, () =>
+const getKey = generateSwrGetKeyFunction(USE_SWR_KEYS.MOVIE_SEARCH);
+
+const useMovieSearch = ({ searchKeyword, language } = {}) => {
+  const { data, error, setSize } = useSWRInfinite(getKey, (_, page) =>
     searchApiService.getMovieSearchResults({ searchKeyword, language, page })
   );
+
+  const handleFetchMoreSearchResults = () => {
+    setSize(prevSize => prevSize + 1);
+  };
+
+  const infiniteScrollTargetRef = useInfiniteScroll(handleFetchMoreSearchResults);
 
   return {
     searchResults: data,
     isLoading: !error && !data,
     isError: error,
+    infiniteScrollTargetRef,
   };
 };
 
