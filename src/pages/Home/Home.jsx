253282c80c508d338ee9fcac52efe8935902c119
nknkcho <srirachacho@gmail.com>
2022-09-08 03:06:23 +0900

    Refactor: 기존에 사용된 PageContainer 삭제

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
-import PageContainer from '@/components/common/PageContainer/PageContainer';
 
 const Home = () => {
-  return (
-    <PageContainer>
-      <div>HOME</div>
-    </PageContainer>
-  );
+  return <div>HOME</div>;
 };
 
 export default Home;
