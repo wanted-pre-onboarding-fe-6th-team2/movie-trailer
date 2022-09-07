042b6103bfefab3fe6a15b19127eb9df635ad0a1
nknkcho <srirachacho@gmail.com>
2022-09-07 16:13:15 +0900

    Refactor: Link 태그를 a 태그로 변경 내부 페이지 이동 목적이 아닌, 외부 사이트 연결 목적이기 때문에 a 태그로 변경하였습니다

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 import facebookLogo from '@/assets/images/facebook.png';
 import instagramLogo from '@/assets/images/instagram.png';
 import twitterLogo from '@/assets/images/twitter.png';
@@ -10,19 +9,19 @@ const Footer = () => {
       <Styled.Wrapper>
         <Styled.SnsList>
           <li>
-            <Link to="https://www.facebook.com/">
+            <a href="https://www.facebook.com/">
               <img src={facebookLogo} alt="Facebook logo" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link to="https://www.instagram.com/">
+            <a href="https://www.instagram.com/">
               <img src={instagramLogo} alt="Instagram logo" />
-            </Link>
+            </a>
           </li>
           <li>
-            <Link to="https://www.twitter.com/">
+            <a href="https://www.twitter.com/">
               <img src={twitterLogo} alt="Twitter logo" />
-            </Link>
+            </a>
           </li>
         </Styled.SnsList>
         <div>Copyright@원티드 프리 온보딩 2조. All rights reserved</div>
