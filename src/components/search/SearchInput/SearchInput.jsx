e3cc73492b8ff18273cbc5426ff6706b88c5fda6
HyoungMin <76457116+hyoungqu23@users.noreply.github.com>
2022-09-07 16:06:05 +0900

    Feat: SearchInput 로직 구현
    - 검색 api 활용해 검색 로직 구현
    - SearchPreview에 전달할 props를 위해 1 page만 활용

--- src/components/search/SearchInput/SearchInput.jsx
+++ src/components/search/SearchInput/SearchInput.jsx
@@ -1,35 +1,63 @@
 import React, { useState } from 'react';
 import searchIcon from '@/assets/svg/searchIcon.svg';
 import * as Styled from '@/components/search/SearchInput/SearchInput.styled';
+import searchApiService from '@/api/searchService';
+import SearchPreview from '@/components/search/SearchPreview/SearchPreview';
 
 const SearchInput = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
 
-  const handleSearchKeywordInputChange = ({ target }) => {
+  const [searchedMovies, setSearchedMovies] = useState([]);
+
+  const searchMovieByKeyword = async keyword => {
+    try {
+      const response = await searchApiService.getMovieSearchResults({
+        searchKeyword: keyword,
+        page: 1,
+      });
+
+      const { results } = response;
+
+      return results;
+    } catch (err) {
+      return err;
+    }
+  };
+
+  const handleSearchKeywordInputChange = async ({ target }) => {
     const { value } = target;
 
     setSearchKeyword(value);
+
+    if (value) {
+      const results = await searchMovieByKeyword(value);
+      setSearchedMovies(results);
+    }
   };
 
   const handleSearchBtnClick = e => {
     e.preventDefault();
 
     // TODO: 검색 로직
+    // searchMovieByKeyword(searchKeyword);
   };
 
   return (
-    <Styled.SearchInputForm>
-      <Styled.SearchInput
-        type="text"
-        placeholder="검색어를 입력하세요."
-        autoComplete="false"
-        value={searchKeyword}
-        onChange={handleSearchKeywordInputChange}
-      />
-      <Styled.SearchButton type="submit" onClick={handleSearchBtnClick}>
-        <img src={searchIcon} alt="검색" />
-      </Styled.SearchButton>
-    </Styled.SearchInputForm>
+    <>
+      <Styled.SearchInputForm>
+        <Styled.SearchInput
+          type="text"
+          placeholder="검색어를 입력하세요."
+          autoComplete="false"
+          value={searchKeyword}
+          onChange={handleSearchKeywordInputChange}
+        />
+        <Styled.SearchButton type="submit" onClick={handleSearchBtnClick}>
+          <img src={searchIcon} alt="검색" />
+        </Styled.SearchButton>
+      </Styled.SearchInputForm>
+      {searchKeyword && <SearchPreview movies={searchedMovies.slice(0, 10)} />}
+    </>
   );
 };
 
