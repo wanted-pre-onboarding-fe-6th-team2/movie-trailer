f6c9b87b72739ebcb312da2413817a745091f5b5
Taak-e <kjtag0408@gmail.com>
2022-09-08 03:21:53 +0900

    Feat: detail 브랜치와 Merge 전 navigate 라우터 로직 작성

--- src/components/home/Card/Card.jsx
+++ src/components/home/Card/Card.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as Styled from '@/components/home/Card/Card.styled';
 
 const { VITE_IMAGE_BASE_URL } = import.meta.env;
 
 const Card = ({ popularMovies: movieInfo }) => {
-  // console.log(popularMovies);
+  const navigate = useNavigate();
+  // console.log(movieInfo);
   const posterUrl = `${VITE_IMAGE_BASE_URL}/${movieInfo.poster_path}`;
 
   const onMoveDetail = () => {
-    alert('hello');
+    const movieId = movieInfo.id;
+    navigate(`/movie-detail/${movieId}`);
   };
 
   return (
