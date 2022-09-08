1b2a4cdbfa61ce353758ceaeed95890575f4911d
jyoon <wldbszp@gmail.com>
2022-09-08 10:55:11 +0900

    Feat: useSWRInfinify을 사용한 SWR api hook 적용
    useSWRInfinify와 useInfinity custom hook을 사용하여 무한 스크롤 시 지속적으로 fetch을 할 수 있도록 적용

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -1,33 +1,36 @@
 import React from 'react';
 import useTopRatedMovies from '@/hooks/api/useTopRatedMovies';
-import * as Styled from '@/pages/NowPlaying/NowPlaying.styled';
+import * as Styled from '@/pages/TopRated/TopRated.styled';
 import MovieListItem from '@/components/MovieListItem/MovieListItem';
 
 const TopRated = () => {
-  const { movies, isError, isLoading } = useTopRatedMovies();
+  const { movies, isError, isLoading, infiniteScrollTargetRef } = useTopRatedMovies();
 
-  if (isError) return <div>Failed to load</div>;
-  if (isLoading) return <div>Loading...</div>;
+  if (isError) return <Styled.StateContain>Failed to load</Styled.StateContain>;
+  if (isLoading) return <Styled.StateContain>Loading...</Styled.StateContain>;
 
-  const movieList = movies.results.map(results => results).flat();
+  const movieList = movies.map(({ results }) => results).flat();
 
   return (
-    <Styled.MoviePage>
-      <Styled.SubTitle>평점순</Styled.SubTitle>
-      <Styled.MovieList>
-        {movieList.map(list => (
-          <MovieListItem
-            key={list.id}
-            id={list.id}
-            imagePath={list.backdrop_path}
-            overview={list.overview}
-            title={list.title}
-            voteAverage={list.vote_average}
-            releaseDate={list.release_date}
-          />
-        ))}
-      </Styled.MovieList>
-    </Styled.MoviePage>
+    <>
+      <Styled.MoviePage>
+        <Styled.SubTitle>평점순</Styled.SubTitle>
+        <Styled.MovieList>
+          {movieList.map(list => (
+            <MovieListItem
+              key={list.id}
+              id={list.id}
+              imagePath={list.poster_path}
+              overview={list.overview}
+              title={list.title}
+              voteAverage={list.vote_average}
+              releaseDate={list.release_date}
+            />
+          ))}
+        </Styled.MovieList>
+      </Styled.MoviePage>
+      <div ref={infiniteScrollTargetRef} />
+    </>
   );
 };
 
