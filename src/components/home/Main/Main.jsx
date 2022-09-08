060227109e014e9767b6af1b03890354f52b1c97
Taak-e <kjtag0408@gmail.com>
2022-09-08 11:24:05 +0900

    Feat: 완료된 infiniteScroll 기능 연결 로직 작성 완료

--- src/components/home/Main/Main.jsx
+++ src/components/home/Main/Main.jsx
@@ -3,32 +3,31 @@ import * as Styled from '@/components/home/Main/Main.styled';
 import Card from '@/components/home/Card/Card';
 import usePopularMovies from '@/hooks/api/usePopularMovies';
 import Loading from '@/components/common/Loading/Loading';
-// import { useInfiniteScroll } from '@/hooks/useInfiniteScroll';
-// import movieApiService from '@/api/movieService';
 
 const Main = () => {
-  const { movies, isLoading, isError } = usePopularMovies();
+  const { movies, isLoading, isError, infiniteScrollTargetRef } = usePopularMovies();
 
-  if (isLoading) return <Loading />;
   if (isError) return <div>오류로 페이지를 불러올 수 없습니다.</div>;
+  if (isLoading) return <Loading />;
 
-  const popularMovies = movies.results;
+  const popularMovieList = movies.map(({ results }) => results).flat();
 
   return (
-    <Styled.Container>
-      <Styled.Wrapper>
-        <Styled.TitleWrapper>
-          <Styled.Title>🌟지금 나에게 필요한 인기 영화는🌟</Styled.Title>
-        </Styled.TitleWrapper>
-        <Styled.CardWrapper>
-          {popularMovies.map((movieInfo, idx) => {
-            return <Card popularMovies={movieInfo} key={idx} />;
-          })}
-          {/* {isLoading && <Loading />} */}
-        </Styled.CardWrapper>
-        {/* {!isLoad && <div ref={setObservationTarget} />} */}
-      </Styled.Wrapper>
-    </Styled.Container>
+    <>
+      <Styled.Container>
+        <Styled.Wrapper>
+          <Styled.TitleWrapper>
+            <Styled.Title>🌟지금 나에게 필요한 인기 영화는🌟</Styled.Title>
+          </Styled.TitleWrapper>
+          <Styled.CardWrapper>
+            {popularMovieList.map((movieInfo, id) => {
+              return <Card popularMovies={movieInfo} key={id} />;
+            })}
+          </Styled.CardWrapper>
+        </Styled.Wrapper>
+      </Styled.Container>
+      <div ref={infiniteScrollTargetRef} />
+    </>
   );
 };
 
