17a7f5e96daa464b8088f8a68499feb5360fd605
jyoon <wldbszp@gmail.com>
2022-09-08 09:36:14 +0900

    Feat: useSWR을 통해 api 호출하도록 수정
    캐싱을 위해 useSWR을 사용

--- src/pages/TopRated/TopRated.jsx
+++ src/pages/TopRated/TopRated.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import useTopRatedMovies from '@/hooks/api/useTopRatedMovies';
 import * as Styled from '@/pages/NowPlaying/NowPlaying.styled';
 import MovieListItem from '@/components/MovieListItem/MovieListItem';
-import PageContainer from '@/components/common/PageContainer/PageContainer';
 
 const TopRated = () => {
-  const getMovieLists = useTopRatedMovies({ page: 10 });
+  const { movies, isError, isLoading } = useTopRatedMovies();
+
+  if (isError) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  const movieList = movies.results.map(results => results).flat();
 
   return (
-    <PageContainer>
-      <Styled.MoviePage>
-        <Styled.SubTitle>평점순</Styled.SubTitle>
-        <Styled.MovieList>
-          {getMovieLists.movies?.results.map(list => (
-            <MovieListItem
-              key={list.id}
-              id={list.id}
-              imagePath={list.backdrop_path}
-              overview={list.overview}
-              title={list.title}
-              voteAverage={list.vote_average}
-              releaseDate={list.release_date}
-            />
-          ))}
-        </Styled.MovieList>
-      </Styled.MoviePage>
-    </PageContainer>
+    <Styled.MoviePage>
+      <Styled.SubTitle>평점순</Styled.SubTitle>
+      <Styled.MovieList>
+        {movieList.map(list => (
+          <MovieListItem
+            key={list.id}
+            id={list.id}
+            imagePath={list.backdrop_path}
+            overview={list.overview}
+            title={list.title}
+            voteAverage={list.vote_average}
+            releaseDate={list.release_date}
+          />
+        ))}
+      </Styled.MovieList>
+    </Styled.MoviePage>
   );
 };
 
--- src/pages/Upcoming/Upcoming.jsx
+++ src/pages/Upcoming/Upcoming.jsx
@@ -2,30 +2,32 @@ import React from 'react';
 import useUpcomingMovies from '@/hooks/api/useUpcomingMovies';
 import * as Styled from '@/pages/NowPlaying/NowPlaying.styled';
 import MovieListItem from '@/components/MovieListItem/MovieListItem';
-import PageContainer from '@/components/common/PageContainer/PageContainer';
 
 const Upcoming = () => {
-  const movieLists = useUpcomingMovies({ page: 1 });
+  const { movies, isError, isLoading } = useUpcomingMovies();
+
+  if (isError) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  const movieList = movies.results.map(results => results).flat();
 
   return (
-    <PageContainer>
-      <Styled.MoviePage>
-        <Styled.SubTitle>개봉예정작</Styled.SubTitle>
-        <Styled.MovieList>
-          {movieLists.movies?.results.map(list => (
-            <MovieListItem
-              key={list.id}
-              id={list.id}
-              imagePath={list.backdrop_path}
-              overview={list.overview}
-              title={list.title}
-              voteAverage={list.vote_average}
-              releaseDate={list.release_date}
-            />
-          ))}
-        </Styled.MovieList>
-      </Styled.MoviePage>
-    </PageContainer>
+    <Styled.MoviePage>
+      <Styled.SubTitle>개봉예정작</Styled.SubTitle>
+      <Styled.MovieList>
+        {movieList.map(list => (
+          <MovieListItem
+            key={list.id}
+            id={list.id}
+            imagePath={list.backdrop_path}
+            overview={list.overview}
+            title={list.title}
+            voteAverage={list.vote_average}
+            releaseDate={list.release_date}
+          />
+        ))}
+      </Styled.MovieList>
+    </Styled.MoviePage>
   );
 };
 
